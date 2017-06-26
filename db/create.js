86e66c608cae1a28bef3286165aad8e976be378b
zcong1993 <1005997919@qq.com>
2017-06-26 15:57:08 +0800

    add seed and db creator

--- db/create.js
+++ db/create.js
@@ -1,6 +1,6 @@
 const db = require('../src/db')
 
-function up() {
+async function up() {
   await db.schema.createTable('users', t => {
     t.increments('id').primary()
     t.string('username', 100)
@@ -8,12 +8,28 @@ function up() {
     t.string('email', 50)
     t.string('avatar_url', 200)
     t.timestamps()
+    t.integer('status').defaultTo(1)
     t.unique('username')
   })
+  await db.schema.createTable('posts', t => {
+    t.increments('id').primary()
+    t.string('title', 200)
+    t.text('description')
+    t.text('content')
+    t.integer('author_id').notNullable().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE')
+    t.integer('status').defaultTo(1)
+    t.timestamps()
+    t.unique('title')
+  })
+  console.log('done')
+  process.exit(0)
 }
 
-function down() {
+async function down() {
   await db.schema.dropTableIfExists('users')
+  await db.schema.dropTableIfExists('posts')
+  console.log('done')
+  process.exit(0)
 }
 
 module.exports = {
--- db/index.js
+++ db/index.js
@@ -1,1 +1,13 @@
+const { up, down } = require('./create')
 
+const args = process.argv
+const command = args[args.length - 1]
+
+switch (command) {
+  case 'up':
+    up()
+    break
+  case 'down':
+    down()
+    break
+}
